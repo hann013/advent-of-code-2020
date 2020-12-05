363617dbc023841a10900f5f67c67ff919ff0ea2
Hannah Zhang <hannahz@fb.com>
2020-12-04 17:03:32 -0700

    remove day 4 prints and add comments

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -40,58 +40,50 @@ def getNumValidPassportsTwo(passports):
             if key == "byr":
                 year = int(val)
                 if year < 1920 or year > 2002:
-                    print("invalid byr", year)
                     isValid = False
                     break
             elif key == "iyr":
                 year = int(val)
                 if year < 2010 or year > 2020:
-                    print("invalid iyr", year)
                     isValid = False
                     break
             elif key == "eyr":
                 year = int(val)
                 if year < 2020 or year > 2030:
-                    print("invalid eyr", year)
                     isValid = False
                     break
             elif key == "hgt":
                 if "cm" in val:
                     height = int(val.split("cm")[0])
                     if height < 150 or height > 193:
-                        print("invalid hgt cm", height)
                         isValid = False
                         break
                 elif "in" in val:
                     height = int(val.split("in")[0])
                     if height < 59 or height > 76:
-                        print("invalid hgt in", height)
                         isValid = False
                         break
                 else:
-                    print("invalid hgt", height)
                     isValid = False
                     break
             elif key == "hcl":
+                # # followed by exactly 6 characters 0-9 or a-f
                 match = re.search("^#([0-9]|[a-f]){6}$", val)
                 if not match:
-                    print("invalid hcl", repr(val))
                     isValid = False
                     break
             elif key == "ecl":
                 if not val in validEyeColors:
-                    print("invalid ecl", val)
                     isValid = False
                     break
             elif key == "pid":
+                # Check that it's a valid number
                 try:
                     int(val)
                 except ValueError:
-                    print("invalid pid", val)
                     isValid = False
                     break
                 if len(val) != 9:
-                    print("invalid pid", val)
                     isValid = False
                     break
 
